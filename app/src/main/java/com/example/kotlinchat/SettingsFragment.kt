7d4613d3ced75e160bc7713ab7c26504486d3073
Nicholas Barkote <barkotenicholas@gmail.com>
2022-07-15 10:18:07 +0300

    Added checkbox preferebce

--- app/src/main/java/com/example/kotlinchat/SettingsFragment.kt
+++ app/src/main/java/com/example/kotlinchat/SettingsFragment.kt
@@ -6,25 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.NavHostFragment
+import androidx.preference.PreferenceFragmentCompat
 import com.example.kotlinchat.databinding.FragmentSettingsBinding
 
 
-class SettingsFragment : Fragment() {
-
-    private lateinit var binding:FragmentSettingsBinding
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        binding = FragmentSettingsBinding.inflate(inflater, container, false)
-
-        binding.root.setOnClickListener {
-            val navHostFragment = activity?.supportFragmentManager?.findFragmentById(R.id.nav_host_frag) as NavHostFragment
-            val navController = navHostFragment.navController
-            val action = SettingsFragmentDirections.actionSettingsToAccSettings()
-            navController.navigate(action)
-        }
-
-        return binding.root
+class SettingsFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.settings,rootKey)
     }
+
+
 }
